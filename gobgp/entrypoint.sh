c5398c6a85129bf8dca647b87757d4611ab5c0ab
Austin Nason <austin.nason@schrodinger.com>
2023-05-03 19:28:12 -0700

    pause pg connection

--- gobgp/entrypoint.sh
+++ gobgp/entrypoint.sh
@@ -1,2 +1,7 @@
-exec /root/gobgp/gobgpd -f /root/gobgp/gobgpd.conf &
-exec /root/gobgp/startup.sh 
+#!/bin/sh
+# to run just gobgpd:
+exec /root/gobgp/gobgpd -f /root/gobgp/gobgpd.conf
+
+## for gobgpd + postgres:
+#exec /root/gobgp/gobgpd -f /root/gobgp/gobgpd.conf &
+#exec /root/gobgp/startup.sh 
--- gobgp/startup.sh
+++ gobgp/startup.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 ## Production
 # /root/gobgp/gobgp monitor global rib -j | /var/tmp/gobgp_to_mongo.py
-/root/gobgp/gobgp monitor global rib -j | /var/tmp/gobgp_to_postgres.py
-##
-## Dev Test
+# /root/gobgp/gobgp monitor global rib -j | /var/tmp/gobgp_to_postgres.py
+
+## Test - read from bgp dump
 # cat /var/tmp/log/bgp.dump.json | /var/tmp/gobgp_to_mongo.py
 # cat /var/tmp/log/new_v4_unicast_bgp.dump.json | /var/tmp/gobgp_to_mongo.py
-##
-### Dev Test - Dump to local file
+
+## Test - Dump to local file
 # /root/gobgp/gobgp monitor global rib -j > /var/tmp/log/bgp.dump.json
